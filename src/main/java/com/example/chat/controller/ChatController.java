2fae45b7409359066a9352964b4e8dff4bb1f808
ibtSdan <ibtsdan@gmail.com>
2025-11-17 15:44:26 +0900

    feat: ChatController 생성

---
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -0,0 +1,20 @@
+package com.example.chat.controller;
+
+import com.example.chat.dto.ChatMessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat/send")
+    public void sendMessage(ChatMessageDto dto){
+        String destination = "/topic/chat"+dto.getRoomId();
+        messagingTemplate.convertAndSend(destination, dto);
+    }
+}
